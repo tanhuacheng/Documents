1629fbfb752c2af44e4305cf11efeeca26e19eef
tanhuacheng <tanhc.gz@gmail.com>
2017-09-26 02:17:24 +0800

    learning rate optimaztion

--- python/deeplearning_ai/deep_neural_networks/deep_neural_networks.py
+++ python/deeplearning_ai/deep_neural_networks/deep_neural_networks.py
@@ -12,6 +12,10 @@ def train(x, y, layers, activations, differentials, loss, dloss, alpha, lambd, i
     assert(np.shape(y)[0] == layers[-1])
     assert(len(layers) == len(activations) == len(differentials))
 
+    beta1 = 0.9
+    beta2 = 0.999
+    epsln = 1e-8
+
     m = np.shape(x)[1]
 
     w = [None]
@@ -26,6 +30,7 @@ def train(x, y, layers, activations, differentials, loss, dloss, alpha, lambd, i
     L = len(l)
 
     da, dz, dw, db = ([None] for i in range(4))
+    vdw, vdb, sdw, sdb = ([0] for i in range(4))
     for i in range(1, L):
         w.append(np.random.randn(l[i], l[i - 1]) * np.sqrt(1 / l[i - 1]))
         b.append(np.random.randn(l[i], 1))
@@ -36,6 +41,15 @@ def train(x, y, layers, activations, differentials, loss, dloss, alpha, lambd, i
         dz.append(0)
         dw.append(0)
         db.append(0)
+
+        vdw.append(0)
+        vdb.append(0)
+        sdw.append(0)
+        sdb.append(0)
+
+    #  idecay = 0
+    #  decay = [[400, 5], [600, 0.5], [1000, 0.4], [7500, 0.5], [15000, 0.2], [-1]]
+    #  decay = [[12500, 0.5], [-1]]
 
     cost = []
     for it in range(iterations):
@@ -52,9 +66,27 @@ def train(x, y, layers, activations, differentials, loss, dloss, alpha, lambd, i
             db[-i] = np.sum(dz[-i], axis=1, keepdims=True) / m
             da[-i - 1] = np.dot(w[-i].T, dz[-i])
 
+            vdw[-i] = beta1 * vdw[-i] + (1 - beta1) * dw[-i]
+            vdb[-i] = beta1 * vdb[-i] + (1 - beta1) * db[-i]
+            sdw[-i] = beta2 * sdw[-i] + (1 - beta2) * np.power(dw[-i], 2)
+            sdb[-i] = beta2 * sdb[-i] + (1 - beta2) * np.power(db[-i], 2)
+
+        c_beta1 = 1 - np.power(beta1, it + 1)
+        c_beta2 = 1 - np.power(beta2, it + 1)
+        #  if it > 0 and (it % 1000) == 0:
+        #      alpha = alpha * 0.95
+        #  if it == decay[idecay][0]:
+        #      alpha *= decay[idecay][1]
+        #      idecay += 1
+
         for i in range(1, L):
-            w[i] = w[i] - alpha * dw[i]
-            b[i] = b[i] - alpha * db[i]
+            c_vdw = vdw[i] / c_beta1
+            c_vdb = vdb[i] / c_beta1
+            c_sdw = sdw[i] / c_beta2
+            c_sdb = sdb[i] / c_beta2
+
+            w[i] = w[i] - alpha * (c_vdw / (np.sqrt(c_sdw) + epsln))
+            b[i] = b[i] - alpha * (c_vdb / (np.sqrt(c_sdb) + epsln))
 
     return cost, w[1:], b[1:]
 
--- python/deeplearning_ai/deep_neural_networks/test.py
+++ python/deeplearning_ai/deep_neural_networks/test.py
@@ -9,13 +9,13 @@ import numpy as np
 from deep_neural_networks import train
 from deep_neural_networks import test
 
-def diff_tanh(x, y):
+def dtanh(x, y):
     return 1 - np.power(y, 2)
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def diff_sigmoid(x, y):
+def dsigmoid(x, y):
     return y * (1 - y)
 
 def loss(x, y):
@@ -23,7 +23,7 @@ def loss(x, y):
     x = np.minimum(x, 0.9999999999)
     return -(y * np.log(x) + (1 - y) * np.log(1 - x))
 
-def diff_loss(x, y):
+def dloss(x, y):
     x = np.maximum(x, 0.0000000001)
     x = np.minimum(x, 0.9999999999)
     return -y / x + (1 - y) / (1 - x)
@@ -31,48 +31,65 @@ def diff_loss(x, y):
 def relu(x):
     return np.maximum(x, 0)
 
-def diff_relu(x, y):
+def drelu(x, y):
     return x > 0
 
 import matplotlib.pyplot as plt
 
+import time
+
 def main():
-    import time
     a = np.loadtxt('../logistic_regression/diabetes_dataset/diabetes_train').T
     x = a[1:]
     y = np.reshape(a[0, :], (1, np.shape(x)[1]))
-
-
-    l = [9, 16, 9, 1]
-    g = [np.tanh, np.tanh, np.tanh, sigmoid]
-    d = [diff_tanh, diff_tanh, diff_tanh, diff_sigmoid]
-    #  np.random.seed(int(time.time()))
-    np.random.seed(0)
-    alpha = 0.4
-    lambd = 0
-
-    tic = time.time()
-    c, w, b = train(x, y, l, g, d, loss, diff_loss, alpha, lambd, 11600)
-    toc = time.time()
-
-    print(toc - tic, l, lambd, alpha, np.sum(c[-8:]) / 8)
-    #  print('\n', w, '\n', b)
-
-    result = (test(x, w, b, g) > 0.5) - y
-    nerror = np.sum(np.abs(result))
-    #  print(result)
-    print(nerror, '%.2f%%' % (100 * nerror / np.shape(y)[1]))
-
 
     a = np.loadtxt('../logistic_regression/diabetes_dataset/diabetes_test').T
     tx = a[1:]
     ty = np.reshape(a[0, :], (1, np.shape(tx)[1]))
 
-    result = (test(tx, w, b, g) > 0.5) - ty
-    nerror = np.sum(np.abs(result))
-    #  print(result)
-    print(nerror, '%.2f%%' % (100 * nerror / np.shape(ty)[1]))
+    r = []
+    for i in range(1, 2):
+        for j in range(1, 2):
+            np.random.seed(0)
 
+            l = [9, 16, 9, 1]
+            g = [np.tanh, np.tanh, np.tanh, sigmoid]
+            d = [dtanh, dtanh, dtanh, dsigmoid]
+            alpha = 0.0012
+            lambd = 0
+            it = 7000
+
+            c, w, b = train(x, y, l, g, d, loss, dloss, alpha, lambd, it)
+            toc = time.time()
+            cost = np.sum(c[-8:]) / 8
+            print(cost, w, l, alpha, lambd, it)
+
+            t = []
+            result = (test(x, w, b, g) > 0.5) - y
+            nerror = np.sum(np.abs(result)) * 100 / np.shape(y)[1]
+            t.append(nerror)
+            print('%.2f%%' % nerror)
+
+            result = (test(tx, w, b, g) > 0.5) - ty
+            nerror = np.sum(np.abs(result)) * 100 / np.shape(ty)[1]
+            t.append(nerror)
+            print('%.2f%%' % nerror)
+
+            t.append(cost)
+            t.append(l)
+            t.append(alpha)
+            t.append(lambd)
+            t.append(it)
+            r.append(t)
+            print()
+
+    print('\n', r, '\n')
+    r.sort(key=lambda x: x[0])
+    print(r[0:2])
+    r.sort(key=lambda x: x[1])
+    print(r[0:2])
+    r.sort(key=lambda x: x[2])
+    print(r[0:2])
 
     plt.plot(c)
     plt.show()
