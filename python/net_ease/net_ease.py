02277c07449689267f86f45ae0db53e4f2c80e32
tanhc <tanhc.gz@gmail.com>
2017-11-10 19:06:20 +0800

    tanhc@ubuntu Fri Nov 10 19:06:20 HKT 2017

--- python/net_ease/net_ease.py
+++ python/net_ease/net_ease.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 # -*- coding:utf-8
 
-'net ease webapi'
+'NetEase API'
 
-__author__ = 'tanhuacheng'
+__version__ = '0.0.1'
 
 import os
 import binascii
 import json
 import base64
-import requests
 import hashlib
+import requests
 
 from Crypto.Cipher import AES
 from http.cookiejar import LWPCookieJar
 
-NET_EASE_URL = 'https://music.163.com/weapi/login/cellphone'
-NONCE        = '0CoJUm6Qyw8W8jud'
-PUBKEY       = '010001'
-MODULUS      = ('00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7'
-                'b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280'
-                '104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932'
-                '575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b'
-                '3ece0462db0a22b8e7')
+NET_EASE_NONCE   = '0CoJUm6Qyw8W8jud'
+NET_EASE_PUBKEY  = '010001'
+NET_EASE_MODULUS = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7' \
+                   'b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280' \
+                   '104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932' \
+                   '575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b' \
+                   '3ece0462db0a22b8e7'
+NET_EASE_AES_IV  = '0102030405060708'
+
+NET_EASE_URL_LOGIN = 'https://music.163.com/weapi/login/cellphone'
+
+COOKIE_PATH = os.path.join(os.path.curdir, '.cookies')
 
 def create_secret_key():
     return binascii.hexlify(os.urandom(8))
@@ -30,28 +34,26 @@ def create_secret_key():
 def aes_encrypt(text, key):
     pad = 16 - len(text) % 16
     text += chr(pad) * pad
-    encypter = AES.new(key, 2, '0102030405060708')
+    encypter = AES.new(key, AES.MODE_CBC, NET_EASE_AES_IV)
     ciphertext = encypter.encrypt(text)
-    ciphertext = base64.b64encode(ciphertext).decode('utf-8')
-    return ciphertext
+    return base64.b64encode(ciphertext).decode('utf-8')
 
 def rsa_encrypt(text, pubkey, modulus):
     text = text[::-1]
     rs = pow(int(binascii.hexlify(text), 16), int(pubkey, 16), int(modulus, 16))
-    #  rs = int(text, 16)**int(pubkey, 16)%int(modulus, 16)
     return format(rs, 'x').zfill(256)
 
 def encrypted_request(text):
     text = json.dumps(text)
     key = create_secret_key()
-    encrypt_text = aes_encrypt(aes_encrypt(text, NONCE), key)
-    encrypt_key = rsa_encrypt(key, PUBKEY, MODULUS)
+    encrypt_text = aes_encrypt(aes_encrypt(text, NET_EASE_NONCE), key)
+    encrypt_key = rsa_encrypt(key, NET_EASE_PUBKEY, NET_EASE_MODULUS)
     return {'params': encrypt_text, 'encSecKey': encrypt_key}
 
 class NetEase(object):
 
     def __init__(self):
-        self.header = {
+        self.headers = {
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -59,27 +61,27 @@ class NetEase(object):
             'Content-Type': 'application/x-www-form-urlencoded',
             'Host': 'music.163.com',
             'Referer': 'https://music.163.com/',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/62.0.3202.75 Chrome/62.0.3202.75 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)' \
+                          ' Ubuntu Chromium/62.0.3202.75 Chrome/62.0.3202.75 Safari/537.36'
         }
 
-        self.cookie_path = os.path.join(os.path.curdir, '.cookies')
         self.session = requests.Session()
-        self.session.cookies = LWPCookieJar(self.cookie_path)
+        self.session.cookies = LWPCookieJar(COOKIE_PATH)
 
-    def phone_login(self, phone, password, remember_login='true'):
+    def login(self, phone, password, remember_login='true'):
         text = {
             'phone': phone,
             'password': hashlib.md5(password.encode()).hexdigest(),
             'rememberLogin': remember_login
         }
-        conn = self.session.post(NET_EASE_URL, data=encrypted_request(text), headers=self.header)
+        data = encrypted_request(text)
+
+        conn = self.session.post(NET_EASE_URL_LOGIN, data=data, headers=self.headers)
         self.session.cookies.save()
 
-        print('conn:', conn)
-        print('conn.text:', conn.text)
+        conn.encoding = 'UTF-8'
 
-        return conn.text
-
+        return json.loads(conn.text)
 
 if __name__ == '__main__':
     import sys
@@ -88,5 +90,4 @@ if __name__ == '__main__':
     phone = input('phone number: ')
     password = getpass('[%s] password for %s: ' % (sys.argv[0], phone));
 
-    net_ease = NetEase()
-    net_ease.phone_login(phone, password)
+    print(NetEase().login(phone, password))
