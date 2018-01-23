03c988c2dfc8a938366c170809aecf3805975f12
tanhuacheng <tanhc.gz@gmail.com>
2018-01-23 18:39:24 +0800

    python/flask: template

--- python/flask/microblog/app/views.py
+++ python/flask/microblog/app/views.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = { 'nickname': 'Tanhc' }
+    posts = [
+            { 'author': { 'nickname': 'John' },
+              'body': 'Beautiful day in Portland!' },
+            { 'author': { 'nickname': 'Susan' },
+              'body': 'The Avengers movie was so cool!'},
+    ]
+    return render_template('index.html',
+        title='Home',
+        user=user,
+        posts=posts)
