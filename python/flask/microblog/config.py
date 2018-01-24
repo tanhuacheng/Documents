82c130ea1c9fc496fa794ecafb60d4e678a5fba7
tanhuacheng <tanhc.gz@gmail.com>
2018-01-24 12:28:52 +0800

    python/flask/microblog: data base

--- python/flask/microblog/config.py
+++ python/flask/microblog/config.py
@@ -8,3 +8,9 @@ OPENID_PROVIDERS = [
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' },
 ]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
