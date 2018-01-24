4f196712ede5827f042e6f3d0ca6aabadd1f9388
tanhuacheng <tanhc.gz@gmail.com>
2018-01-24 15:35:27 +0800

    python/flask/microblog: user login

--- python/flask/microblog/app/__init__.py
+++ python/flask/microblog/app/__init__.py
@@ -5,4 +5,14 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from app import views, models
--- python/flask/microblog/app/models.py
+++ python/flask/microblog/app/models.py
@@ -6,6 +6,21 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
