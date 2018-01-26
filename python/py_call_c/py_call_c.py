9ed923ec1716f402c0024dc7c19b9ac2b662fb7f
tanhuacheng <tanhc.gz@gmail.com>
2018-01-26 11:30:49 +0800

    tanhc@tanhc-samsung 2018年 01月 26日 星期五 11:30:49 CST

--- python/py_call_c/py_call_c.py
+++ python/py_call_c/py_call_c.py
@@ -6,6 +6,16 @@ from ctypes import *
 
 lib = cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), 'libtest.so'))
 
+# Note: Make sure you keep references to CFUNCTYPE() objects as long as they are used from C code.
+# ctypes doesn't, and if you don't, they may be garbage collected, crashing your program when a
+# callback is made.
+#
+# Also, note that if the callback function is called in a thread created outside of Python's control
+# (e.g. by the foreign code that calls the callback), ctypes creates a new dummy Python thread on
+# every invocation. This behavior is correct for most purposes, but it means that values stored with
+# threading.local will not survive across different callbacks, even when those calls are made from
+# the same C thread.
+
 CB_FUNC = CFUNCTYPE(None)
 
 def foo():
