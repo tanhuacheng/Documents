959e96997489144ba65e6c601293d4eacf00ad93
tanhuacheng <tanhc.gz@gmail.com>
2018-01-29 11:46:43 +0800

    tanhc@tanhc-samsung 2018年 01月 29日 星期一 11:46:43 CST

--- python/flask/neteasy/hello.py
+++ python/flask/neteasy/hello.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8
 
 import os, sys
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import net_ease
 from getpass import getpass
 
@@ -15,35 +15,79 @@ if not ne.is_login():
     ne.login(phone, password)
 
 recommends = ne.get_recommend()
+for i in range(0, len(recommends)):
+    try:
+        recommends[i]['url'] = ne.get_musics_url([recommends[i]['id']])[0]['url']
+    except:
+        pass
 
 playlist_details = []
 playlists = ne.get_playlists()
 for l in playlists:
-    playlist_details.append(ne.get_playlist_detail(l['id'], l['trackCount']))
+    detail = ne.get_playlist_detail(l['id'], l['trackCount'])
+    for i in range(0, len(detail)):
+        try:
+            detail[i]['url'] = ne.get_musics_url([detail[i]['id']])[0]['url']
+        except:
+            pass
+    playlist_details.append(detail)
 
 @app.route('/')
+@app.route('/index')
 def index():
     albums = []
 
     album = { 'name': '每日推荐', 'musics': [] }
     for r in recommends:
-        music = {}
-        music['name'] = r['name']
-        music['picUrl'] = r['album']['picUrl']
-        music['url'] = ne.get_musics_url([r['id']])[0]['url']
-        album['musics'].append(music)
+        if r['url']:
+            music = {}
+            music['name'] = r['name']
+            music['picUrl'] = r['album']['picUrl']
+            #  music['url'] = ne.get_musics_url([r['id']])[0]['url']
+            music['url'] = r['url']
+            album['musics'].append(music)
     albums.append(album)
 
-    album = { 'name': '我喜欢的音乐', 'musics': [] }
-    for r in playlist_details[0]:
-        music = {}
-        music['name'] = r['name']
-        music['picUrl'] = r['al']['picUrl']
-        music['url'] = ne.get_musics_url([r['id']])[0]['url']
-        album['musics'].append(music)
-    albums.append(album)
+    try:
+        album = { 'name': playlists[0]['name'], 'musics': [] }
+        for r in playlist_details[0]:
+            if r['url']:
+                music = {}
+                music['name'] = r['name']
+                music['picUrl'] = r['al']['picUrl']
+                #  music['url'] = ne.get_musics_url([r['id']])[0]['url']
+                music['url'] = r['url']
+                album['musics'].append(music)
+        albums.append(album)
+    except:
+        pass
 
     return render_template('index.html', title='Home', albums=albums)
+
+@app.route('/refresh')
+def refresh():
+    global recommends
+    global playlist_details
+    global playlists
+
+    recommends = ne.get_recommend()
+    for i in range(0, len(recommends)):
+        try:
+            recommends[i]['url'] = ne.get_musics_url([recommends[i]['id']])[0]['url']
+        except:
+            pass
+
+    playlist_details = []
+    playlists = ne.get_playlists()
+    for l in playlists:
+        detail = ne.get_playlist_detail(l['id'], l['trackCount'])
+        for i in range(0, len(detail)):
+            try:
+                detail[i]['url'] = ne.get_musics_url([detail[i]['id']])[0]['url']
+            except:
+                pass
+        playlist_details.append(detail)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
