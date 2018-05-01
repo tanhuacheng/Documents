566d5fc37673f4f6e0d82208b0ae59cab64df3bb
tanhuacheng <tanhc.gz@gmail.com>
2018-05-01 17:22:57 +0800

    use https instead of git

--- github/clone.sh
+++ github/clone.sh
@@ -1,3 +1,3 @@
 #sh
-git clone git@github.com:tanhuacheng/gencode.git
-git clone git@github.com:tanhuacheng/tanlib.git
+git clone https://github.com/tanhuacheng/gencode.git
+git clone https://github.com/tanhuacheng/tanlib.git
